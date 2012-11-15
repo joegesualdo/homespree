2444d8f4e3afe53dabc6032adb64ed5ad61f044d
Joe Gesualdo <joegesualdo@gmail.com>
2012-11-14 20:18:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homespree::Application.config.secret_token = '6803e266fd4ce27915de8e972d9d7b0e97b0baa55a363e869b08e9a54f95908dc76608bf89d856c2a65a4bb79469a665404b57452a84cd28b342576ae41347a0'
